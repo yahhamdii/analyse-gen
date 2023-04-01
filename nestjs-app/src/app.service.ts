b616731113585a3f56320fa27547b7971284eee6
Hamdi Yahyaoui <hamdi_yahyaoui@yahoo.fr>
2023-04-01 19:38:29 +0200

    first commit

---
+++ nestjs-app/src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+
+@Injectable()
+export class AppService {
+  constructor(private dataSource: DataSource) {}
+
+  getUsers() {
+    return this.dataSource.query('SELECT * FROM users');
+  }
+}
---
+++ nextjs-app/next.config.js
@@ -0,0 +1,7 @@
+webpack: config => {
+  config.watchOptions = {
+    poll: 1000,
+    aggregateTimeout: 300,
+  }
+  return config
+}
